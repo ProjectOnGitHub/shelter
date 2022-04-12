14c230263397adecbbe6b18cbb0d8a0f55d57c44
Aleksandr <soft@vhlv.ru>
2022-04-12 20:52:05 +0300

    refactor: replace js files

--- shelter/pages/index.js
+++ shelter/pages/index.js
@@ -1,3 +1,23 @@
+const menu = document.querySelector('.header__menu');
+const menuButton = document.querySelector('.header__menu-button');
+
+const toggleMenuButton = () => {
+  menu.classList.toggle('header__menu_opened');
+  menuButton.classList.contains('header__menu-button_theme_light')
+    ? menuButton.classList.toggle('header__menu-button_theme_light_opened')
+    : menuButton.classList.toggle('header__menu-button_opened');
+};
+
+const closeMenu = (e) => {
+  if (e.target.classList.contains('header__menu-link')) {
+    menuButton.classList.remove('header__menu-button_theme_light_opened');
+    menu.classList.remove('header__menu_opened');
+  }
+};
+
+menuButton.addEventListener('click', toggleMenuButton);
+menu.addEventListener('click', closeMenu);
+
 const url = '../../assets/utils/pets.json';
 
 async function getPets() {
