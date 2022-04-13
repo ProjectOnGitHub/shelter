90d31c6eadec61810326988f7c82ae50f10792c9
Aleksandr <soft@vhlv.ru>
2022-04-13 16:56:06 +0300

    refactor: fix canceled cards changes

--- shelter/pages/index.js
+++ shelter/pages/index.js
@@ -19,42 +19,42 @@ const closeMenu = (e) => {
 menuButton.addEventListener('click', toggleMenuButton);
 menu.addEventListener('click', closeMenu);
 
-const url = '../../assets/utils/pets.json';
+// const url = '../../assets/utils/pets.json';
 
-async function getPets() {
-  const res = await fetch(url);
-  const data = await res.json();
-  return data.forEach((item) => {
-    const card = new Card(item.name, item.img);
+// async function getPets() {
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   return data.forEach((item) => {
+//     const card = new Card(item.name, item.img);
 
-    const cardElement = card.generateCard();
+//     const cardElement = card.generateCard();
 
-    document.querySelector('.cards').append(cardElement);
-  });
-}
+//     document.querySelector('.cards').append(cardElement);
+//   });
+// }
 
-class Card {
-  constructor(name, img) {
-    this.name = name;
-    this.img = img;
-  }
+// class Card {
+//   constructor(name, img) {
+//     this.name = name;
+//     this.img = img;
+//   }
 
-  getTemplate() {
-    const cardElement = document
-      .querySelector('.cards__template')
-      .content
-      .querySelector('.cards__item')
-      .cloneNode(true);
-    return cardElement;
-  }
+//   getTemplate() {
+//     const cardElement = document
+//       .querySelector('.cards__template')
+//       .content
+//       .querySelector('.cards__item')
+//       .cloneNode(true);
+//     return cardElement;
+//   }
 
-  generateCard() {
-    this.element = this.getTemplate();
-    this.element.querySelector('.cards__image').src = this.img;
-    this.element.querySelector('.cards__image').alt = this.name;
-    this.element.querySelector('.cards__caption').textContent = this.name;
-    return this.element;
-  }
-}
+//   generateCard() {
+//     this.element = this.getTemplate();
+//     this.element.querySelector('.cards__image').src = this.img;
+//     this.element.querySelector('.cards__image').alt = this.name;
+//     this.element.querySelector('.cards__caption').textContent = this.name;
+//     return this.element;
+//   }
+// }
 
-getPets();
+// getPets();
