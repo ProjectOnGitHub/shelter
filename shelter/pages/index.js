5016bd289623ebac8f0f13ef398337a1513c9440
Aleksandr <soft@vhlv.ru>
2022-04-22 19:47:24 +0300

    feat: add a counter of change cards in different resolutions on the main page

--- shelter/pages/index.js
+++ shelter/pages/index.js
@@ -6,12 +6,45 @@ import pets from '../assets/utils/pets.js';
 // const url = '../../assets/utils/pets.json';
 
 const popup = document.querySelector('.popup');
-const popupButtonOpen = document.querySelector('.cards');
+const cardsList = document.querySelector('.cards');
 
 const popupButtonClose = document.querySelector('.popup__close-button');
 
 const menu = document.querySelector('.header__menu');
 const menuButton = document.querySelector('.header__menu-button');
+
+const buttonPrev = document.querySelector('.slider__button_prev');
+const buttonNext = document.querySelector('.slider__button_next');
+
+function movePrevSlide() {
+  getRandomCard();
+  cardsList.classList.add('transition-prev');
+  buttonPrev.removeEventListener('click', movePrevSlide);
+  buttonNext.removeEventListener('click', moveNextSlide);
+}
+
+function moveNextSlide() {
+  getRandomCard();
+  cardsList.classList.add('transition-next');
+  buttonPrev.removeEventListener('click', movePrevSlide);
+  buttonNext.removeEventListener('click', moveNextSlide);
+}
+
+function removeTransitionPrev() {
+  cardsList.classList.remove('transition-prev');
+  buttonPrev.addEventListener('click', movePrevSlide);
+}
+function removeTransitionNext() {
+  cardsList.classList.remove('transition-next');
+  buttonNext.addEventListener('click', moveNextSlide);
+}
+
+cardsList.addEventListener('animationend', removeTransitionNext);
+
+cardsList.addEventListener('animationend', removeTransitionPrev);
+
+buttonPrev.addEventListener('click', movePrevSlide);
+buttonNext.addEventListener('click', moveNextSlide);
 
 const toggleMenuButton = () => {
   menu.classList.toggle('header__menu_opened');
@@ -38,28 +71,58 @@ const addCard = (card) => {
   document.querySelector('.cards').append(cardElement);
 };
 
-const createCard = () => {
-  pets.forEach((item, i) => {
-    if (i < 3) {
-      const card = new Card(
-        item.name,
-        item.img,
-        item.breed,
-        item.type,
-        item.description,
-        item.age,
-        item.inoculations,
-        item.diseases,
-        item.parasites,
-      );
-
-      return addCard(card);
-    }
+const createCard = (arr) => {
+  removeCards();
+  arr.forEach((index) => {
+    pets.forEach((item, i) => {
+      if (i === index) {
+        const card = new Card(
+          item.name,
+          item.img,
+          item.breed,
+          item.type,
+          item.description,
+          item.age,
+          item.inoculations,
+          item.diseases,
+          item.parasites,
+        );
+        return addCard(card);
+      }
+    });
   });
 };
 
-createCard();
+function removeCards() {
+  const cards = document.querySelectorAll('.cards__item');
+  cards.forEach((item) => item.remove());
+}
 
+function getRandomCard() {
+  let counter;
+
+  const currentPage = document.location.pathname;
+  if (window.matchMedia('(min-width: 1280px)').matches) {
+    currentPage.includes('main')
+      ? counter = -3
+      : counter = 0;
+  } else if (window.matchMedia('(min-width: 768px)').matches && window.matchMedia('(max-width: 1279.2px)').matches) {
+    currentPage.includes('main')
+      ? counter = -2
+      : counter = 1;
+  } else {
+    currentPage.includes('main')
+      ? counter = -1
+      : counter = 2;
+  }
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7]
+    .sort(() => 0.5 - Math.random())
+    .slice(counter).sort();
+
+  createCard(arr);
+}
+
+getRandomCard();
 function openPopup(e) {
   const currentElement = e.target.closest('.cards__item').querySelector('.cards__caption').textContent;
   addInfoToPopup(currentElement);
@@ -72,7 +135,7 @@ function closePopup(e) {
     popup.classList.remove('popup_opened');
   }
 }
-popupButtonOpen.addEventListener('click', openPopup);
+cardsList.addEventListener('click', openPopup);
 popupButtonClose.addEventListener('click', closePopup);
 popup.addEventListener('click', closePopup);
 
